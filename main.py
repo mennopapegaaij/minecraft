669aa11d64e61d5d530a3b95ed934875f42232fa
Menno Papegaaij <mennopapegaaij@users.noreply.github.com>
2026-06-22 08:11:21 +0200

    3D wereld met Ursina: blokken plaatsen en afbreken

--- main.py
+++ main.py
@@ -1,131 +1,117 @@
-import pygame
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
 import random
 
+app = Ursina()
+
 # --- Instellingen ---
-SCHERM_BREEDTE = 800
-SCHERM_HOOGTE = 600
-BLOK_GROOTTE = 40  # Hoe groot elk blok is in pixels
+WERELD_GROOTTE = 20   # De wereld is 20 x 20 blokken groot
+WERELD_DIEPTE = 5     # Hoe veel lagen de grond heeft
 
 # --- Kleuren van de blokken ---
-KLEUR_LUCHT = (135, 206, 235)   # Lichtblauw
-KLEUR_GRAS = (106, 170, 60)     # Groen
-KLEUR_AARDE = (134, 96, 67)     # Bruin
-KLEUR_STEEN = (128, 128, 128)   # Grijs
-KLEUR_BEDROCK = (50, 50, 50)    # Donkergrijs
-KLEUR_RAND = (0, 0, 0)          # Zwart (randen van blokken)
+KLEUR_GRAS = color.rgb(106, 170, 60)
+KLEUR_AARDE = color.rgb(134, 96, 67)
+KLEUR_STEEN = color.rgb(128, 128, 128)
 
-# --- Soorten blokken (als nummers) ---
-LUCHT = 0
-GRAS = 1
-AARDE = 2
-STEEN = 3
-BEDROCK = 4
+# Lijst van alle blokken in de wereld
+blokken = []
 
-# --- Hoe groot de wereld is ---
-WERELD_BREEDTE = 100   # 100 blokken breed
-WERELD_HOOGTE = 50     # 50 blokken hoog
+# Welk type blok de speler in de hand heeft
+huidig_blok = 'gras'
+
+
+class Blok(Button):
+    """Een blok in de wereld. Je kunt erop klikken om het af te breken of er naast te plaatsen."""
+
+    def __init__(self, positie, blok_type='gras'):
+        # Kies de kleur op basis van het type blok
+        if blok_type == 'gras':
+            kleur = KLEUR_GRAS
+        elif blok_type == 'aarde':
+            kleur = KLEUR_AARDE
+        else:
+            kleur = KLEUR_STEEN
+
+        super().__init__(
+            parent=scene,
+            position=positie,
+            model='cube',           # Een kubus (blok)
+            origin_y=0.5,
+            texture='white_cube',   # Witte textuur met randen
+            color=kleur,
+            highlight_color=color.lime,  # Wordt groen als je erop kijkt
+        )
+        self.blok_type = blok_type
+        blokken.append(self)
+
+    def input(self, toets):
+        if self.hovered:  # Als de muis over dit blok gaat
+            if toets == 'left mouse down':
+                # Linker muisknop: blok afbreken
+                blokken.remove(self)
+                destroy(self)
+
+            if toets == 'right mouse down':
+                # Rechter muisknop: nieuw blok plaatsen naast dit blok
+                nieuw_blok = Blok(
+                    positie=self.position + mouse.normal,
+                    blok_type=huidig_blok
+                )
 
 
 def maak_wereld():
-    """Maakt een nieuwe wereld met blokken."""
-    # Begin met een lege wereld (alles is lucht)
-    wereld = [[LUCHT for _ in range(WERELD_BREEDTE)] for _ in range(WERELD_HOOGTE)]
+    """Maakt de 3D wereld met blokken."""
+    for x in range(WERELD_GROOTTE):
+        for z in range(WERELD_GROOTTE):
+            # Kies een willekeurige hoogte voor het landschap
+            hoogte = random.randint(0, 3)
 
-    for x in range(WERELD_BREEDTE):
-        # Kies een willekeurige hoogte voor het landschap
-        grond_hoogte = random.randint(20, 28)
+            # Bovenste laag = gras
+            Blok(positie=(x, hoogte, z), blok_type='gras')
 
-        for y in range(WERELD_HOOGTE):
-            if y == grond_hoogte:
-                wereld[y][x] = GRAS       # Bovenste laag = gras
-            elif y < grond_hoogte + 4:
-                wereld[y][x] = AARDE      # Daarna 4 lagen aarde
-            elif y < WERELD_HOOGTE - 1:
-                wereld[y][x] = STEEN      # De rest is steen
-            else:
-                wereld[y][x] = BEDROCK    # Onderste laag = bedrock
-
-    return wereld
+            # Lagen aarde daaronder
+            for y in range(hoogte - 1, hoogte - WERELD_DIEPTE, -1):
+                if y > hoogte - 3:
+                    Blok(positie=(x, y, z), blok_type='aarde')
+                else:
+                    Blok(positie=(x, y, z), blok_type='steen')
 
 
-def teken_wereld(scherm, wereld, camera_x, camera_y):
-    """Tekent alle zichtbare blokken op het scherm."""
-    # Hoeveel blokken passen er op het scherm?
-    blokken_x = SCHERM_BREEDTE // BLOK_GROOTTE + 2
-    blokken_y = SCHERM_HOOGTE // BLOK_GROOTTE + 2
+def input(toets):
+    global huidig_blok
 
-    # Begin bij het eerste zichtbare blok
-    start_x = camera_x // BLOK_GROOTTE
-    start_y = camera_y // BLOK_GROOTTE
+    # Kies welk blok je in de hand hebt
+    if toets == '1':
+        huidig_blok = 'gras'
+        print("Je hebt nu: GRAS")
+    if toets == '2':
+        huidig_blok = 'aarde'
+        print("Je hebt nu: AARDE")
+    if toets == '3':
+        huidig_blok = 'steen'
+        print("Je hebt nu: STEEN")
 
-    for y in range(start_y, min(start_y + blokken_y, WERELD_HOOGTE)):
-        for x in range(start_x, min(start_x + blokken_x, WERELD_BREEDTE)):
-            blok = wereld[y][x]
-
-            # Kies de juiste kleur
-            if blok == LUCHT:
-                continue  # Lucht hoeven we niet te tekenen
-            elif blok == GRAS:
-                kleur = KLEUR_GRAS
-            elif blok == AARDE:
-                kleur = KLEUR_AARDE
-            elif blok == STEEN:
-                kleur = KLEUR_STEEN
-            elif blok == BEDROCK:
-                kleur = KLEUR_BEDROCK
-
-            # Bereken waar op het scherm dit blok staat
-            scherm_x = x * BLOK_GROOTTE - camera_x
-            scherm_y = y * BLOK_GROOTTE - camera_y
-
-            # Teken het blok
-            pygame.draw.rect(scherm, kleur, (scherm_x, scherm_y, BLOK_GROOTTE, BLOK_GROOTTE))
-            # Teken een randje eromheen
-            pygame.draw.rect(scherm, KLEUR_RAND, (scherm_x, scherm_y, BLOK_GROOTTE, BLOK_GROOTTE), 1)
+    # Druk op Escape om af te sluiten
+    if toets == 'escape':
+        quit()
 
 
-def main():
-    pygame.init()
-    scherm = pygame.display.set_mode((SCHERM_BREEDTE, SCHERM_HOOGTE))
-    pygame.display.set_caption("Mijn Minecraft!")
-    klok = pygame.time.Clock()
+# --- Maak de wereld ---
+maak_wereld()
 
-    # Maak de wereld aan
-    wereld = maak_wereld()
+# --- De speler ---
+speler = FirstPersonController()
+speler.position = (WERELD_GROOTTE // 2, 6, WERELD_GROOTTE // 2)  # Zet de speler bovenop de wereld
 
-    # Camera positie (hoeveel pixels zijn we al naar rechts/beneden)
-    camera_x = 0
-    camera_y = 0
-    CAMERA_SNELHEID = 10
+# --- Lucht kleur ---
+sky = Sky()
 
-    # Spel-lus
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
+# --- Uitleg op het scherm ---
+uitleg = Text(
+    text="[1] Gras  [2] Aarde  [3] Steen\nLinks klikken = afbreken  Rechts klikken = plaatsen",
+    position=(-0.85, 0.45),
+    scale=1.2,
+    background=True,
+)
 
-        # Beweeg de camera met de pijltjestoetsen
-        toetsen = pygame.key.get_pressed()
-        if toetsen[pygame.K_LEFT]:
-            camera_x -= CAMERA_SNELHEID
-        if toetsen[pygame.K_RIGHT]:
-            camera_x += CAMERA_SNELHEID
-        if toetsen[pygame.K_UP]:
-            camera_y -= CAMERA_SNELHEID
-        if toetsen[pygame.K_DOWN]:
-            camera_y += CAMERA_SNELHEID
-
-        # Zorg dat de camera niet buiten de wereld gaat
-        camera_x = max(0, min(camera_x, WERELD_BREEDTE * BLOK_GROOTTE - SCHERM_BREEDTE))
-        camera_y = max(0, min(camera_y, WERELD_HOOGTE * BLOK_GROOTTE - SCHERM_HOOGTE))
-
-        # Teken alles
-        scherm.fill(KLEUR_LUCHT)  # Lucht achtergrond
-        teken_wereld(scherm, wereld, camera_x, camera_y)
-
-        pygame.display.flip()
-        klok.tick(60)  # 60 frames per seconde
-
-
-main()
+app.run()
